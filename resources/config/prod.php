4f353a6d1e89b439e605d7911eb65227e29dfa13
Leevi Graham <leevi@newism.com.au>
2012-07-16 00:52:41 +1000

    Updated config settings

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -2,17 +2,15 @@
 
 $app['config.github'] = array(
     'user' => 'leevigraham',
-    'client_id' => 'a6f356e693fb28987533',
-    'client_secret' => '444281f23d938bab0f1857c91009a57e93b70ea2',
-    'access_token' => '223b5c41b9eb210ed9c9a77c7c85306f23445b55',
+    'client_id' => '',
+    'client_secret' => '',
+    'access_token' => '',
     'api_url' => 'https://api.github.com',
-    // 'callback_url' => $app['url_generator']->generate('hooks_process', array(), true),
     'callback_url' => 'http://requestb.in/1e5ae8z1',
     'callback_secret' => '868yw2CzuHE[xzF6ePPosbTvRDmYRw',
     'repos' => array(
-        'leevigraham/jenkins-test',
-        'leevigraham/jquery-bigTarget.js'
+        'leevigraham/github-pr-code-sniffer'
     ),
 );
 
-$app['config.event_folder_path'] = realpath(__DIR__ . "/../../../events");
+$app['config.event_folder_path'] = realpath(__DIR__ . "/../../events");
